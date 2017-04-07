7cbb7a792c70b9dfb74d6c283f080223ebbd75fc
Aidan Anderson <aidan@floqast.com>
2017-04-06 18:25:46 -0700

    Added ability to bind to custom host address

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 npm i -g simple-reverse-proxy-cli
 
-simple-proxy --ssl --target http://localhost:3000 --port 8888
+simple-proxy --ssl --target http://localhost:3000 --host http://localhost:8080
 
 > Starting proxy server...
-> Forwarding requests at port 8888 to target http://localhost:3000
+> Forwarding requests at localhost:8080 to target http://localhost:3000
--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var argv = require('yargs').argv;
+var url = require('url');
 var fs = require('fs');
 var pem = require('pem');
 var httpProxy = require('http-proxy');
@@ -9,10 +10,31 @@ var async = require('async');
 var options = {};
 var tasks = [];
 
+var target = argv.target;
+var host = argv.host;
+
 async.waterfall([
     function verifyTarget(callback) {
-        if (!argv.target) {
-            return callback(new Error('--target option in form of url must be provided.'));
+        if (!target) {
+            return callback(new Error('--target option in form of url must be provided.  e.g. http://localhost:8008'));
+        }
+        return callback(null);
+    },
+    function verifyHost(callback) {
+        if (!argv.host) {
+            console.log('Defaulting host to http://localhost:8080');
+            host = {
+                hostname: 'localhost',
+                port: '8080'
+            };
+            return callback(null);
+        }
+
+        host = url.parse(host);
+
+        if (!host.hostname ||
+            !host.port) {
+            throw new Error('--host option in form of url must be provided.  e.g. http://localhost:8008')
         }
         return callback(null);
     },
@@ -44,7 +66,7 @@ async.waterfall([
     },
     function startReverseProxyServer(callback) {
         console.log('Starting proxy server...');
-        var proxy = httpProxy.createServer(options).listen(options.port);
+        var proxy = httpProxy.createServer(options).listen(host.port, host.hostname);
 
         return callback(null, proxy);
     }
@@ -54,5 +76,5 @@ function (err, proxy) {
         throw new Error(err);
     }
 
-    console.log('Forwarding requests at port ' + options.port + ' to target ' + options.target);
+    console.log('Forwarding requests at ' + host.hostname + ':' + host.port + ' to target ' + options.target);
 });
